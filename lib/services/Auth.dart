7941cf42e1c05ebb0857cc66bf4ca3188ed79504
Felipe Trova de Araujo <fetrova@gmail.com>
2019-09-10 22:29:44 +0200

    Sign In with Google

--- lib/services/Auth.dart
+++ lib/services/Auth.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class User {
   User({@required this.uid});
@@ -11,6 +12,7 @@ abstract class AuthBase {
   Future<User> currentUser();
   Future<User> signInAnonymously();
   Future<void> signOut();
+  Future<User> signInWithGoogle();
   Stream<User> get onAuthStateChanged;
 }
 
@@ -32,10 +34,38 @@ class Auth implements AuthBase {
     return _userFromFirebase(firebaseUser);
   }
 
+  /**
+   * Sign in with Google
+   */
+  Future<User> signInWithGoogle() async {
+    var googleSignIn = GoogleSignIn();
+    GoogleSignInAccount googleUser = await googleSignIn.signIn();
+
+    if (googleUser != null) {
+      GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+
+      if (googleAuth.idToken != null && googleAuth.accessToken != null) {
+        AuthResult authResult = await _firebaseAuth.signInWithCredential(
+            GoogleAuthProvider.getCredential(
+                idToken: googleAuth.idToken,
+                accessToken: googleAuth.accessToken));
+        FirebaseUser firebaseUser = authResult.user;
+        return _userFromFirebase(firebaseUser);
+      } else {
+        throw StateError('Missing Google Auth Token');
+      }
+    } else {
+      throw StateError('Google sign in aborted');
+    }
+  }
+
   Future<void> signOut() async {
     return await _firebaseAuth.signOut();
   }
 
+  /**
+   * Transform a Firebase user in a app level User object
+   */
   User _userFromFirebase(FirebaseUser firebaseUser) {
     if (firebaseUser == null) {
       return null;
